e8053da9fc09b54c63d555ae2640941241f208a8
S M A Tousi <mohamadali.tousi@gmail.com>
2025-01-18 10:40:47 -0600

    latent regularity added

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,10 +5,10 @@
 # method: ['unimatch_v2', 'fixmatch', 'supervised']
 # exp: just for specifying the 'save_path'
 # split: ['92', '1_16', ...]. Please check directory './splits/$dataset' for concrete splits
-dataset='pascal'
+dataset='ade20k'
 method='unimatch_v2'
 exp='dinov2_small'
-split='366'
+split='1_2'
 
 config=configs/${dataset}.yaml
 labeled_id_path=splits/$dataset/$split/labeled.txt
